5ee2757c78ba9456cd31339afbb664a630260e9a
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2023-11-02 16:52:12 -0500

    cambios de qr angello

--- src/pages/conferencia-curso/index.tsx
+++ src/pages/conferencia-curso/index.tsx
@@ -4,19 +4,56 @@ import { Catalog } from "../../types/Catalog";
 import { PythonShell } from "python-shell";
 import { ResponsivePage } from "../../components/ResponsivePage";
 
-const VerCatalogo = ({ catalog }: { catalog: Catalog }) => {
-  const { catalogs } = useCatalogs();
+import React, { useEffect } from 'react';
 
+const VerCatalogo: React.FC<{ catalog: any }> = ({ catalog }) => {
+  useEffect(() => {
+    const domReady = (fn: Function) => {
+      if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+      } else {
+        //@ts-ignore
+        document.addEventListener("DOMContentLoaded", fn);
+      }
+    };
+
+    domReady(() => {
+      const myqr = document.getElementById('your-qr-result');
+      let lastResult: string;
+      let countResults = 0;
+
+      const onScanSuccess = (decodeText: string, decodeResult: any) => {
+        if (decodeText !== lastResult) {
+          ++countResults;
+          lastResult = decodeText;
+          alert("Su QR es: " + decodeText);
+
+          if (myqr) {
+            myqr.innerHTML = ` you scan ${countResults} : ${decodeText} `;
+          }
+        }
+      };
+
+      const htmlscanner = new (window as any).Html5QrcodeScanner(
+        "my-qr-reader", { fps: 10, qrbox: 250 }
+      );
+
+      htmlscanner.render(onScanSuccess);
+    });
+
+  }, []);
 
 
   return (
 
     <ResponsivePage>
       
+      <button>escanear</button>
       <div id="your-qr-result"></div>
       <div className="my-qr-reader-cont">
         <div id="my-qr-reader" className="my-qr-reader"></div>
       </div>
+
         <script src="https://unpkg.com/html5-qrcode"></script>
         
     </ResponsivePage>
@@ -26,32 +63,3 @@ const VerCatalogo = ({ catalog }: { catalog: Catalog }) => {
 };
 
 export default VerCatalogo;
-function domReady(fn: () => void): void {
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-      setTimeout(fn, 1);
-  } else {
-      document.addEventListener("DOMContentLoaded", fn);
-  }
-}
-
-domReady(() => {
-  const myqr = document.getElementById('your-qr-result');
-  let lastResult: string | undefined, countResults = 0;
-
-  const onScanSuccess = (decodeText: string, decodeResult: any) => {
-      if (decodeText !== lastResult) {
-          ++countResults;
-          lastResult = decodeText;
-          alert("Su QR es: " + decodeText);
-          
-          if (myqr) {
-              myqr.innerHTML = ` you scan ${countResults} : ${decodeText} `;
-          }
-      }
-  }
-//@ts-ignore
-  const htmlscanner = new (Html5QrcodeScanner as any)(
-      "my-qr-reader", { fps: 10, qrbox: 250 }
-  );
-  htmlscanner.render(onScanSuccess);
-});
