b229a5ead2caebaec69a4e89bb4289274099af0e
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2023-11-02 15:23:54 -0500

    cambio el api de local a heroku

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:1338/api'
+    baseURL: 'https://shrieking-web-97943-0c89be05ca8d.herokuapp.com/api'
 });
